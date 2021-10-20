01e52108c25e2ca343b0a6ff046dbc44e0bfaf06
hamza-boudouche <boudouche.hamza.13.07.2000@gmail.com>
2021-10-20 11:41:48 +0100

    fixed available dates bug

--- setDates.js
+++ setDates.js
@@ -5,7 +5,7 @@ setCache();
 
 (async () => {
 	//vendredi
-	for (let i = 19; i <= 23; i++) {
+	for (let i = 19; i < 23; i++) {
 		await insertAvailableDate(new Date(2021, 9, 22, i, 0))
 		await insertAvailableDate(new Date(2021, 9, 22, i, 15))
 		await insertAvailableDate(new Date(2021, 9, 22, i, 30))
@@ -13,29 +13,31 @@ setCache();
 	}
 
 	//samedi
-	for (let i = 10; i <= 12; i++) {
+	for (let i = 10; i < 12; i++) {
 		await insertAvailableDate(new Date(2021, 9, 23, i, 0))
 		await insertAvailableDate(new Date(2021, 9, 23, i, 15))
 		await insertAvailableDate(new Date(2021, 9, 23, i, 30))
 		await insertAvailableDate(new Date(2021, 9, 23, i, 45))
 	}
 
-	for (let i = 14; i <= 18; i++) {
+	for (let i = 14; i < 18; i++) {
 		await insertAvailableDate(new Date(2021, 9, 23, i, 0))
 		await insertAvailableDate(new Date(2021, 9, 23, i, 15))
 		await insertAvailableDate(new Date(2021, 9, 23, i, 30))
 		await insertAvailableDate(new Date(2021, 9, 23, i, 45))
 	}
+
+	await insertAvailableDate(new Date(2021, 9, 23, 12, 15))
 
 	//dimanche
-	for (let i = 10; i <= 12; i++) {
+	for (let i = 10; i < 12; i++) {
 		await insertAvailableDate(new Date(2021, 9, 24, i, 0))
 		await insertAvailableDate(new Date(2021, 9, 24, i, 15))
 		await insertAvailableDate(new Date(2021, 9, 24, i, 30))
 		await insertAvailableDate(new Date(2021, 9, 24, i, 45))
 	}
 
-	for (let i = 14; i <= 18; i++) {
+	for (let i = 14; i < 18; i++) {
 		await insertAvailableDate(new Date(2021, 9, 24, i, 0))
 		await insertAvailableDate(new Date(2021, 9, 24, i, 15))
 		await insertAvailableDate(new Date(2021, 9, 24, i, 30))
